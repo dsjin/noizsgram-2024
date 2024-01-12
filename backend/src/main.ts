ccbd1a49413893123b0efdf39ca452fa0b7f67c1
Thatchakon Jom-ud <thatchakon.jomud@jp.honda-ri.com>
2024-01-12 11:48:05 +0900

    chore: added Swagger Document

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder().setTitle('NoizsGram')
+      .setDescription(
+        'The cloned Instagram for education purpose.',
+      )
+      .addBearerAuth()
+      .build()
+  )
+  SwaggerModule.setup('api', app, document)
+  app.enableCors()
   await app.listen(3000)
 }
 bootstrap()
